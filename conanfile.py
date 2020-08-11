8e95af4f3cc74b2b74ae9fe9cb0cf455f0fc5af5
WuMo <wumo@outlook.com>
2020-08-11 21:07:52 +0800

    [release]

---
+++ conanfile.py
@@ -0,0 +1,23 @@
+from conans import CMake, ConanFile, tools
+import os
+
+class MagicGetConan(ConanFile):
+    name = "magic_get"
+    version = "1.0.1"
+    url = "https://github.com/apolukhin/magic_get"
+    description = "std::tuple like methods for user defined types without any macro or boilerplate code"
+    
+    settings = "os", "compiler", "build_type", "arch"
+    
+    no_copy_source = True
+    
+    def source(self):
+        tools.get(f"{self.url}/archive/{self.version}.tar.gz")
+    
+    def package(self):
+        include_folder = os.path.join(
+            self.source_folder, f'{self.name}-{self.version}', 'include')
+        self.copy(pattern='*.hpp', dst='include', src=include_folder)
+    
+    def package_id(self):
+        self.info.header_only()
